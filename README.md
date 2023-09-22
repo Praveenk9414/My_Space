b14b20b9eaabf4e9f6a8af6124327d4e7f5f6498
Praveenk9414 <praveenk9414@gmail.com>
2023-09-22 19:50:19 +0530

    Add new java program

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # My_Space
 <br>
-Hey there how u doing.
+Hey there how u doing.(Praveen this side)
---
+++ test_1.java
@@ -0,0 +1,6 @@
+import java.util.*;
+public class test_1{
+    public static void main(String args[]){
+        System.out.println("Hello World!");
+    }
+}
