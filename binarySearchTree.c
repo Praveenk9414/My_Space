977ef77a702b7077e4e7d0341151021313aadee4
Praveenk9414 <praveenk9414@gmail.com>
2024-02-11 22:02:33 +0530

    Program to check whether the inorder traversal of the BST is in ascending order

--- binarySearchTree.c
+++ binarySearchTree.c
@@ -10,36 +10,39 @@ typedef struct node {
 
 // Function to create a new node with the given data
 node *createNode(int data) {
-    node *newNode = malloc(sizeof(node));
-    newNode->data = data;
-    newNode->leftNode = NULL;
-    newNode->rightNode = NULL;
-    return newNode;
+    node *newNode = malloc(sizeof(node));  // Allocate memory for the new node
+    newNode->data = data;  // Set the data of the new node
+    newNode->leftNode = NULL;  // Initialize left child pointer to NULL
+    newNode->rightNode = NULL;  // Initialize right child pointer to NULL
+    return newNode;  // Return the newly created node
 }
 
-// Function for in-order traversal
+// Function for in-order traversal of the binary tree
 void InOrder(node *ptr) {
-    if (ptr != NULL) {
-        InOrder(ptr->leftNode);
-        printf("%d ", ptr->data);
-        InOrder(ptr->rightNode);
+    if (ptr != NULL) {  // Check if the current node is not NULL
+        InOrder(ptr->leftNode);  // Recursively traverse the left subtree
+        printf("%d ", ptr->data);  // Print the data of the current node
+        InOrder(ptr->rightNode);  // Recursively traverse the right subtree
     }
 }
-int isBST(node *ptr){
-    static node *prev = NULL;
-    if(ptr != NULL){
-        if(!isBST(ptr->leftNode)){
-            return 0;
+
+// Function to check if a binary tree is a binary search tree (BST)
+int isBST(node *ptr) {
+    static node *prev = NULL;  // Static variable to store the previous node during traversal
+    if (ptr != NULL) {  // Check if the current node is not NULL
+        if (!isBST(ptr->leftNode)) {  // Recursively check the left subtree
+            return 0;  // Return 0 if the left subtree is not a BST
         }
-        if(prev != NULL && ptr->data <= prev->data){
-            return 0;
+        if (prev != NULL && ptr->data <= prev->data) {  // Check if the current node violates the BST property
+            return 0;  // Return 0 if the BST property is violated
         }
-        prev = ptr;
-        return isBST(ptr->rightNode);
-    }else{
-        return 1;
+        prev = ptr;  // Update the previous node
+        return isBST(ptr->rightNode);  // Recursively check the right subtree
+    } else {
+        return 1;  // Return 1 if the subtree is empty (i.e., it is a BST)
     }
 }
+
 int main() {
     // Create nodes for the binary tree
     node *H1 = createNode(10);
@@ -58,13 +61,16 @@ int main() {
     H3->leftNode = H6;
     H3->rightNode = H7;
 
+    // Perform in-order traversal of the binary tree
     printf("In-order traversal: ");
     InOrder(H1);
     printf("\n");
-    if(isBST(H1) == 1){
-        printf("Yes it is inorder\n");
-    }else{
-        printf("No it is not inorder\n");
+
+    // Check if the binary tree is a BST
+    if (isBST(H1) == 1) {
+        printf("Yes, it is a binary search tree (BST).\n");
+    } else {
+        printf("No, it is not a binary search tree (BST).\n");
     }
 
     return 0;
